ffe8ae27976c880f81ccd8754cdbbeda0b3b14c2
wangmingsheng <ipconfig_121@163.com>
2016-09-05 09:46:19 +0800

    hello android

--- GitTest.java
+++ GitTest.java
@@ -2,6 +2,7 @@ public class GitTest {
 	public static final void main(String[] args) {
 		long start = System.currentTimeMills();
 		System.out.println("Hello World !");
+		System.out.println("Hello Android !");
 		System.out.println(System.currentTimeMills() - start);
 	}
 }
